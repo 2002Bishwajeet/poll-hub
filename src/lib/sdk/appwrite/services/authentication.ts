304a38afeb8ae922c4e98d29eb92b1712b6bb98c
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2023-07-27 17:50:50 +0530

    remove localhost

--- src/lib/sdk/appwrite/services/authentication.ts
+++ src/lib/sdk/appwrite/services/authentication.ts
@@ -33,10 +33,10 @@ export default class AppwriteAuthentication extends AuthenticationBase {
         this.account.deleteSession("current");
     }
     public loginWithDiscord(): void {
-        this.account.createOAuth2Session("discord", 'http://localhost:3000/#/home', window.location.href);
+        this.account.createOAuth2Session("discord", `${window.location.href}/home`, window.location.href);
     }
     public loginWithGithub(): void {
-        this.account.createOAuth2Session("github", 'http://localhost:3000/#/home', window.location.href);
+        this.account.createOAuth2Session("github", `${window.location.href}/home`, window.location.href);
     }
     public async signInAnonymously(): Promise<Session> {
         const response = await this.account.createAnonymousSession();
